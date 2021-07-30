6269c80b74950c60068c0d4405c1c9507e8f596e
Oleg Mandrychenko <oleg_mandrychenko@epam.com>
2021-07-30 17:20:32 +0300

    init task

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# tomcat-nginx-lb
-task
+# Tomcat(backend) and Nginx(frontend) in MIGs with LB.
+
+## Task goals
+
+1. Create bucket for Tomcat application files and another one for static web files (think about permissions)
+2. Create MIG for backend with installed tomcat and on boot download demo application from bucket. Setup autoscaling by CPU (think about scale down)
+3. Create bucket for Nginx application files and another one for static web files (think about permissions)
+4. Add one more MIG for frontend with nginx, by path /demo/ show demo app from bucket, by path /img/picture.jpg show file from bucket
+5. Create LB for tomcat and nginx
+6. Setup export of nginx logs to bucket/BigQuery
+7. Make SSL terination
+
+## Solution
+1. 
+* Making bucket for tomcat through gcloud.
+
+```
+
+```
+
+2. 
+* Create instance template for tomcat MIG with startup script from bucket. (used startup.sh file in repo)
+
+```
+gcloud beta compute --project=tomcat-nginx-lb instance-templates create tomcat-template1 --machine-type=e2-medium --network=projects/tomcat-nginx-lb/global/networks/default --network-tier=PREMIUM --metadata=startup-script-url=gs://tomcat-bucket1/start.sh --maintenance-policy=MIGRATE --service-account=1010500951238-compute@developer.gserviceaccount.com --scopes=https://www.googleapis.com/auth/cloud-platform --tags=http-server,https-server --image=debian-10-buster-v20210721 --image-project=debian-cloud --boot-disk-size=10GB --boot-disk-type=pd-balanced --boot-disk-device-name=tomcat-template1 --no-shielded-secure-boot --no-shielded-vtpm --no-shielded-integrity-monitoring --reservation-affinity=any
+```
+
+* Create Managed Instance Group from tomcat-template
+
+```
+
+```
+
+4. 
+* Create instance template for nginx MIG with startup script from bucket. (used startup-nginx.sh file in repo)
+
+```
+
+```
---
+++ startup.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+# Install java and tomcat, configure user for tomcat
+sudo su <<HERE
+apt update
+apt install wget -y
+apt install default-jdk -y
+useradd -m -U -d /opt/tomcat -s /bin/false tomcat
+wget https://downloads.apache.org/tomcat/tomcat-10/v10.0.8/bin/apache-tomcat-10.0.8.tar.gz
+tar -zxvf apache-tomcat-*.tar.gz -C /opt/tomcat --strip-components=1
+chown tomcat:tomcat /opt/tomcat/ -R
+chmod u+x /opt/tomcat/bin -R
+
+
+# Systemd Config tomcat.service
+cat << EOF > /etc/systemd/system/tomcat.service
+[Unit]
+Description=Apache Tomcat
+Wants=network.target
+After=network.target
+
+[Service]
+Type=forking
+
+Environment=JAVA_HOME=/usr/lib/jvm/java-1.11.0-openjdk-amd64/
+Environment=CATALINA_PID=/opt/tomcat/temp/tomcat.pid
+Environment=CATALINA_HOME=/opt/tomcat
+Environment='CATALINA_OPTS=-Xms512M -Xmx1G -Djava.net.preferIPv4Stack=true'
+Environment='JAVA_OPTS=-Djava.awt.headless=true'
+
+ExecStart=/opt/tomcat/bin/startup.sh
+ExecStop=/opt/tomcat/bin/shutdown.sh
+SuccessExitStatus=143
+
+User=tomcat
+Group=tomcat
+UMask=0007
+RestartSec=10
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+# Edit tomcat-users.xml
+cat << EOF > /opt/tomcat/conf/tomcat-users.xml
+<?xml version='1.0' encoding='utf-8'?>
+<tomcat-users>
+<role rolename="admin-gui, manager-gui" />
+<user username="admin" password="admin" roles="manager-gui,admin-gui"/>
+</tomcat-users>
+EOF
+
+
+# Editing remote access to manager (Server-status, manager app)
+cat << EOF > /opt/tomcat/webapps/manager/META-INF/context.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<Context antiResourceLocking="false" privileged="true" >
+  <CookieProcessor className="org.apache.tomcat.util.http.Rfc6265CookieProcessor"
+                   sameSiteCookies="strict" />
+  <Valve className="org.apache.catalina.valves.RemoteAddrValve"
+         allow="127\.\d+\.\d+\.\d+|::1|0:0:0:0:0:0:0:1|.*" />
+  <Manager sessionAttributeValueClassNameFilter="java\.lang\.(?:Boolean|Integer|Long|Number|String)|org\.apache\.catalina\.filters\.CsrfPreventionFilter\$LruCache(?:\$1)?|java\.util\.(?:Linked)?HashMap"/>
+</Context>
+EOF
+
+
+# Editing remote access to host-manager
+cat << EOF > /opt/tomcat/webapps/host-manager/META-INF/context.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<Context antiResourceLocking="false" privileged="true" >
+  <CookieProcessor className="org.apache.tomcat.util.http.Rfc6265CookieProcessor"
+                   sameSiteCookies="strict" />
+  <Valve className="org.apache.catalina.valves.RemoteAddrValve"
+         allow="127\.\d+\.\d+\.\d+|::1|0:0:0:0:0:0:0:1|.*" />
+  <Manager sessionAttributeValueClassNameFilter="java\.lang\.(?:Boolean|Integer|Long|Number|String)|org\.apache\.catalina\.filters\.CsrfPreventionFilter\$LruCache(?:\$1)?|java\.util\.(?:Linked)?HashMap"/>
+</Context>
+EOF
+
+
+# Restart daemon and start service
+systemctl daemon-reload
+systemctl start tomcat
+systemctl enable tomcat
+
+
+# Install app. Access to app by address <ExternalIP>:8080/sample/
+wget https://tomcat.apache.org/tomcat-7.0-doc/appdev/sample/sample.war
+mv sample.war /opt/tomcat/webapps
+systemctl restart tomcat
+HERE
